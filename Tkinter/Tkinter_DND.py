9985f352a5e9d4795226f2690f0e603be942bf5a
Inty Killa Raymi <cesar.cardozo.ud@gmail.com>
2018-10-03 10:09:34 -0500

    Procedure Modified

--- Tkinter/Tkinter_DND.py
+++ Tkinter/Tkinter_DND.py
@@ -1,23 +1,51 @@
-import tkinter as tk
+from tkinter import *
 
-class Window:
-    root = None
-    choose_menu = None
-    drag_canvas = None
-    def __init__(self):
-        self.root = tk.Tk()
-        aux = tk.Label(self.root, text = "Choose")
-        aux.pack()
-        self.drag_canvas = tk.Frame(self.root, height = 200, bg="cyan")
-        aux = tk.Label(self.drag_canvas, text = "Drag and Drop")
-        self.drag_canvas.pack(fill = "both", expand = True)
-        
-        
-    def SetTitle(self, name):
-        self.root.title(str(name))
+class DragManager():
+    def add_dragable(self, widget):
+        widget.bind("<ButtonPress-1>", self.on_start)
+        widget.bind("<B1-Motion>", self.on_drag)
+        widget.bind("<ButtonRelease-1>", self.on_drop)
+        widget.configure(cursor="hand1")
 
-class Object:
-    def __init__(self, tipo, pos):
-        print("Tipo: " + tipo + " - Posición: "+ pos)
+    def on_start(self, event):
+        print("Clic")
+        pass
 
-window  = Window()
+    def on_drag(self, event):
+        # you could use this method to move a floating window that
+        # represents what you're dragging
+        pass
+
+    def on_drop(self, event):
+        # find the widget under the cursor
+        x,y = event.widget.winfo_pointerxy()
+        target = event.widget.winfo_containing(x,y)
+        try:
+            target.configure(image=event.widget.cget("image"))
+        except:
+            pass
+
+root = Tk()
+root.geometry("840x480")
+
+frame = Frame(root, height=480, width=200, bg="red")
+canvas = Canvas(root, height=480, width=640, bg="cyan")
+frame.propagate(0)
+
+
+#image = PhotoImage(file="C:/Users/Shivam/Pictures/Paint/Body.png")
+
+#label = Label(canvas, image=image)
+#label.pack()
+
+label_2 = Label(frame, text="Drop Here !")
+dnd = DragManager()
+dnd.add_dragable(label_2)
+label_2.pack()
+label_2.place(x=200, y=225, anchor=CENTER)
+
+canvas.pack(side=RIGHT)
+frame.pack()
+
+root.mainloop()
+
